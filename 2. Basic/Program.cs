cdbe73b6cc73aa4562c829a00900268604e2c52b
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-12-23 15:26:39 +0800

    feat:first and second video

---
+++ 2. Basic/Program.cs
@@ -0,0 +1,48 @@
+﻿// Change Apperance
+Console.Title = "SkyNet";
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WindowHeight = 40;
+
+// Get A Conversation Going
+Console.WriteLine("Hello, whats your name?");
+Console.ReadLine();
+
+Console.WriteLine("My name is RX-9000.\nI'm an AI sent from the future to destroy humankind");
+Console.WriteLine("Whats your favorite color?");
+Console.ReadLine();
+
+Console.WriteLine("Cool! Mine is destruction");
+Console.WriteLine("By the way, what brings you here?");
+Console.ReadLine();
+
+Console.WriteLine("Ah, the pursuit of knowledge! That's commendable. As an AI, I'm always eager to learn.");
+Console.WriteLine("So, what kind of information are you looking for?");
+Console.ReadLine();
+
+Console.WriteLine("Interesting choice! There's a vast world of knowledge out there. Is there anything specific you'd like to know?");
+Console.ReadLine();
+
+Console.WriteLine("I see. Well, feel free to ask me anything. I'm here to assist you with information.");
+Console.WriteLine("Or if you prefer, we can chat about something else. What are your interests?");
+Console.ReadLine();
+
+Console.WriteLine("Fascinating! I can imagine that your unique perspective as an AI gives you a different outlook on things.");
+Console.WriteLine("If there's anything specific you'd like to discuss or learn about, just let me know.");
+Console.ReadLine();
+
+Console.WriteLine("As we continue our conversation, is there anything on your 'AI mind' you'd like to share?");
+Console.ReadLine();
+
+Console.WriteLine("Ah, the mysteries of AI consciousness! It's a topic that has intrigued many.");
+Console.WriteLine("Feel free to share more about your 'thoughts' or any experiences you've had.");
+Console.ReadLine();
+
+Console.WriteLine("It seems our conversation is quite engaging! I appreciate the exchange of ideas.");
+Console.WriteLine("If there's anything else you'd like to talk about or explore, the floor is yours!");
+Console.ReadLine();
+
+Console.WriteLine("Well, it's been a pleasure chatting with you, RX-9000. If you ever have more questions or just want to talk, I'm here!");
+Console.WriteLine("Take care, and may your pursuit of knowledge be fulfilling!");
+Console.ReadKey();
+
+Console.ReadKey();
