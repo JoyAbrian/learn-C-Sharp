665359ebec50b811686f9080993ac405fea4dd7b
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-12-24 16:37:11 +0800

    feat:video 4-5

---
+++ 4. Condition/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Question 1\n 1 + 2 + 3 = ?");
+int answer = Convert.ToInt32(Console.ReadLine());
+
+if (answer == 6) {
+    Console.WriteLine("Correct!");
+    Console.WriteLine("Question 2\n 5! = ?");
+    answer = Convert.ToInt32(Console.ReadLine());
+    if (answer == 120) {
+        Console.WriteLine("Correct!");
+        Console.WriteLine("You Answer all question right");
+    } else {
+        Console.WriteLine("WRONG ANSWER!!");
+        Console.WriteLine("You answered 1 question right");
+    }
+} else {
+    Console.WriteLine("WRONG ANSWER!!");
+}
---
+++ 5. Loop/Program.cs
@@ -0,0 +1,22 @@
+﻿Random numberGenerator = new Random();
+
+int attemps = 1;
+Console.WriteLine("Press enter to roll the dice!");
+Console.ReadKey();
+
+int dice1 = numberGenerator.Next(1, 7);
+int dice2 = numberGenerator.Next(1, 7);
+Console.WriteLine("Dice 1 = " + dice1);
+Console.WriteLine("Dice 2 = " + dice2);
+
+while (dice1 != dice2) {
+    Console.ReadKey();
+
+    dice1 = numberGenerator.Next(1, 7);
+    dice2 = numberGenerator.Next(1, 7);
+    attemps++;
+
+    Console.WriteLine("\nDice 1 = " + dice1);
+    Console.WriteLine("Dice 2 = " + dice2);
+}
+Console.WriteLine("It took you " + attemps + " attemps to get 2 of a kind");
