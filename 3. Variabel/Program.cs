1db9390e79eee960698673815ba9a32961b19d65
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-12-24 15:16:29 +0800

    feat: video 3

---
+++ 3. Variabel/Program.cs
@@ -0,0 +1,18 @@
+﻿double num1;
+double num2;
+double num3;
+
+Console.Write("Input the first number: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input the second number: ");
+num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input the third number: ");
+num3 = Convert.ToDouble(Console.ReadLine());
+
+double multiply = num1 * num2 * num3;
+Console.WriteLine("The multiply is " + multiply);
+
+double avg = (num1 + num2 + num3)/3;
+Console.WriteLine("The average is " + avg);
