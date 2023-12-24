95564543eb269c195cfd64b8b3e33062f22e5f33
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-12-24 16:54:01 +0800

    feat: video 6

---
+++ 6. Array/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("How many students are in your class: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+string[] students = new string[length];
+Console.WriteLine("Please input the names of the students");
+
+for (int i = 0; i < students.Length; i++) {
+    students[i] = Console.ReadLine();
+}
+
+Array.Sort(students);
+Console.WriteLine("------------------");
+
+for (int i = 0; i < students.Length; i++) {
+    Console.WriteLine(students[i]);
+}
