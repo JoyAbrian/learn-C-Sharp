c22de80df43d5165f2baf4769f0bc8d2db17c27d
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-12-24 19:00:24 +0800

    feat: video 7

---
+++ 7. Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+
+            Program program = new Program();
+            program.wordCount(sentence);
+        }
+
+        void wordCount(string sentence) {
+            int wordCount = sentence.Split(" ").Length;
+            Console.WriteLine("There are " + wordCount + " words in that sentences");
+        }
+    }
+}
